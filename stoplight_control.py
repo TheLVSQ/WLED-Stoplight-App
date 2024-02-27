7314d2d46dd28537370509eaabd61719f5ac1cf7
Luke Levesque <Luke.Levesque@fullyaccountable.com>
2024-02-26 21:24:29 -0500

    reworked, simplie logic

--- stoplight_control.py
+++ stoplight_control.py
@@ -6,89 +6,78 @@ import sys
 import os
 import logging
 from datetime import datetime, timedelta
-from time import ctime
 
-# Define the next change time based on current time
-def next_change_time(current_time):
-    # Calculate next minute for changes between green, yellow, and red
-    if current_time.minute < 59:
-        # If current minute is less than 59, next change is the next minute
-        next_change = current_time + timedelta(minutes=1)
-        next_change = next_change.replace(second=0)  # Reset seconds to 0 for the start of the minute
-    else:
-        # If current minute is 59, next change is at the top of the next hour
-        next_change = (current_time + timedelta(hours=1)).replace(minute=0, second=0)
-    return next_change
+# Set up logging at info level
+logging.basicConfig(level=logging.INFO)
 
-#get the current time
-cur_time = datetime.now()
-
-
-#log the time and date of start up
-logging.basicConfig(filename='stoplight.log', level=logging.INFO) 
-logging.info(f"Time: {cur_time.strftime('%H:%M:%S')}, Date: {time.strftime('%m/%d/%Y')}")
-logging.info("Stoplight control script started")
-
-#open the config json file
-with open('wled_config.json') as json_file:
-    config = json.load(json_file)
+#Open the config file
+with open('wled_config.json') as f:
+    config = json.load(f)
 
 #create a new instance of the WLED class
 stoplight = wled.Wled_Strip(config['server_ip'], config['start_time'], config['end_time'])
 
-#This script needs to run in a loop during the start and end times from the config file. 
-#They are stored as strings in "HH:MM:SS" format, so we need to convert them to datetime objects
-start_time = datetime.strptime(config['start_time'], '%H:%M:%S')
-end_time = datetime.strptime(config['end_time'], '%H:%M:%S')
+def get_current_time():
+    #Get the current time from the system without the date and microseconds
+    current_time = datetime.now().time().replace(microsecond=0)
+    return current_time
 
-#If execution begins before the start time, wait until the start time
+#From the stoplight object, get the start and end times
+start_time = stoplight.start_time
+end_time = stoplight.end_time
+
+#convert the start and end times to datetime objects
+start_time = datetime.strptime(start_time, '%H:%M:%S').time()
+end_time = datetime.strptime(end_time, '%H:%M:%S').time()
+
+#This while loop checks to see if the current time is less than the start time. If it is, it will sleep for 5 seconds and check again
 while True:
-    #update the current time
-    current_time = datetime.now()
+    cur_time = get_current_time()
 
-    #compare the current time to the start time
-    if current_time < start_time:
-        print(f"Waiting for start time: {start_time}")
+    if cur_time < start_time:
+        print(f'Current time is {cur_time}. Waiting for {start_time}')
         time.sleep(5)
     else:
         break
-    
 
-#run the loop while the current time is between the start and end times
-while current_time >= start_time and current_time <= end_time:
-    #update the current time
-    current_time = datetime.now()
-    next_change = next_change_time(current_time)
-    
-    print(f"Next change: {next_change}")
-    
-    current_hour = current_time.hour
-    current_minute = current_time.minute
-    current_second = current_time.second
+#This while loop checks the values of the minute and seconds of the current time.
+#If the minute integer equals certain values, it will perform actions on the stoplight object
+cur_time = get_current_time()
 
-    print(current_time)
-    logging.info(f"Time: {current_time}")
+while start_time <= cur_time <= end_time:
+    cur_time = get_current_time()
     
-    #if the minute and second of the current time is between XX:01:00 and XX:01:59, turn on the green light
-    
-    if 1 <= current_minute <= 58:
-        stoplight.greenlight()
-        print("Green light on")
-        
-    elif current_minute == 59:
-        stoplight.yellowlight()
-        print("Yellow light on")
+    print(f'Current time is {get_current_time()}')
 
-    elif current_minute == 0:
+    if cur_time.minute == 0:
         stoplight.redlight()
-        print("Red light on")
-        
-    # Calculate sleep duration as the difference between now and the next change time
-    sleep_duration = (next_change - datetime.now()).total_seconds()
-    print(f"Sleeping for {sleep_duration} seconds until the next change.")
-    time.sleep(sleep_duration)
+        print("Red Light called")
+        logging.info(f"Red Light called at {get_current_time()}")
+        #calculate the number of seconds until minute 1 and zero seconds and sleep for that amount of time
+        sleep_time = 60 - get_current_time().second
+        time.sleep(sleep_time)
+    
+    elif 1 <= cur_time.minute <= 58:
+        stoplight.greenlight()
+        print(f"Green Light called at {get_current_time()}")
+        logging.info(f"Green Light called at {get_current_time()}")
+        #calculate the number of seconds until the 59th minute of the hour and sleep for that amount of time
+        sleep_time = 3540 - (get_current_time().minute * 60 + cur_time.second)
+        time.sleep(sleep_time)
+        print(f"Sleeping for {sleep_time} seconds")
+    elif cur_time.minute == 59:
+        stoplight.yellowlight()
+        print(f"Yellow Light called at {get_current_time()}")
+        logging.info(f"Yellow Light called at {get_current_time()}")
+        #calculate the number of seconds until the next hour and sleep for that amount of time
+        sleep_time = 60 - get_current_time().second
+        time.sleep(sleep_time)
+        print(f"Sleeping for {sleep_time} seconds")
+    
+    cur_time = get_current_time()
+
 
 #turn off the lights
 stoplight.shutdown()
 logging.info("Stoplight control script stopped at " + time.strftime('%H:%M:%S'))
-
+print("Stoplight control script stopped at " + time.strftime('%H:%M:%S'))
